0354035028af5825617888e3642ed1f22e1e32af
kiran-c8 <kiranc8298@gmail.com>
2023-07-27 10:41:43 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import 'Routes' instead of 'Switch'
 import Navbar from './components/Navbar';
-import LandingPage from './components/LandingPage';
+// import LandingPage from './components/LandingPage';
 import store from './redux/store';
 
 function App() {

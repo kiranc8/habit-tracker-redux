519fe73ee792e33172788a584155666c4dc5d111
kiran-c8 <kiranc8298@gmail.com>
2023-07-27 11:07:15 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import 'Routes' instead of 'Switch'
 import Navbar from './components/Navbar';
-// import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage';
 import store from './redux/store';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <Provider store={store}>
       <Router basename='habit-tracker-redux/'>
           <Routes>
-            {/* <Route exact path="/" element={<LandingPage />} /> */}
+            <Route exact path="/" element={<LandingPage />} />
             <Route path="/" element={<Navbar/>}/>
           </Routes>
       </Router>
